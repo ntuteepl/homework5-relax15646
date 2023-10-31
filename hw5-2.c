0449d13db310c66d9cfcb6213fc6d9d62b610c54
relax15646 <146065194+relax15646@users.noreply.github.com>
2023-10-31 15:02:59 +0800

    hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+main()
+{
+int time;
+double money;
+scanf("%d",&time);
+if(time>0&&time<=800)
+    money=time*0.9;
+
+else if(time>800&&time<1500)
+    money=time*0.9*0.9;
+
+else if(time>=1500)
+    money=time*0.9*0.9*0.79;
+
+printf("%.1f",money);
+
+
+}
 
